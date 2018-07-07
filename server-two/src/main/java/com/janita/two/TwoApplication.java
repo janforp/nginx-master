46e6df4a3b230c3d5db90453884cf1cea7166800
hspcadmin <880902>
2018-07-08 01:55:03 +0800

    负载均衡

---
+++ server-two/src/main/java/com/janita/two/TwoApplication.java
@@ -0,0 +1,17 @@
+package com.janita.two;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created on 2018/7/8
+ *
+ * @author Janita
+ */
+@SpringBootApplication
+public class TwoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwoApplication.class,args);
+    }
+}
